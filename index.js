793db1ec9ec5896ffae98ae00ef102f2682c50df
Dennis1-1995 <83753035+Dennis1-1995@users.noreply.github.com>
2021-08-10 10:45:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// Deel 1:
+const age = 18;
+
+if(age>=18){
+    console.log('Je mag naar binnen.');
+}
+else{
+    console.log('Helaas, je bent te jong');
+}
+
+// Deel 2:
+const isFemale = true;
+
+if (isFemale){
+    console.log('Je mag tickets kopen');
+}else{
+    console.log('Je mag niet naar binnen');
+}
+
+// Deel 3:
+const driverStatus = 'Bob';
+
+if (driverStatus == 'Bob'){
+    console.log('Je mag rijden');
+}else{
+    console.log('Je mag niet rijden');
+}
+
